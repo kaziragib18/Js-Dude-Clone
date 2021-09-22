412f16735d3a92e20df06dc7f877a580d274556f
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-09-22 23:31:40 +0600

    data added dynamically

--- main.js
+++ main.js
@@ -1,38 +1,53 @@
-const tutorialData=[
+const tutorialData = [
       {
-            title:" Interview Questions",
-            desc:"If you want to upgrade the person shouting at you in every evening at 5.00pm, you have to be competent and pretty sharp with latest technology. The world is competitive. but dont worry, that js dude will compile the questions for you. this will make your life easier to win the next big thing."
+            title: " Interview Questions",
+            desc: "If you want to upgrade the person shouting at you in every evening at 5.00pm, you have to be competent and pretty sharp with latest technology. The world is competitive. but dont worry, that js dude will compile the questions for you. this will make your life easier to win the next big thing."
       },
       {
-            tittle:"50 Dev tool Tips",
-            desc:"If you have some idea about dev tool and want to become a master..this is the right video for your"
+            title: "50 Dev tool Tips",
+            desc: "If you have some idea about dev tool and want to become a master..this is the right video for your"
       },
       {
-            tittle:"scope & closure",
-            desc:"Understand scope, scope chain, hoisting, closure in JavaScript in the most easiest and funniest way. This is so easy even your grandma can understand."
+            title: "scope & closure",
+            desc: "Understand scope, scope chain, hoisting, closure in JavaScript in the most easiest and funniest way. This is so easy even your grandma can understand."
       },
       {
-            tittle:"Interview Questions",
-            desc:"If you want to upgrade the person shouting at you in every evening at 5.00pm, you have to be competent and pretty sharp with latest technology. The world is competitive. but dont worry, that js dude will compile the questions for you. this will make your life easier to win the next big thing."
+            title: "Interview Questions",
+            desc: "If you want to upgrade the person shouting at you in every evening at 5.00pm, you have to be competent and pretty sharp with latest technology. The world is competitive. but dont worry, that js dude will compile the questions for you. this will make your life easier to win the next big thing."
       },
       {
-            tittle:"Angular Interview Question",
-            desc:"A complete guideline to prepare for angular interviews. Also, you can use these questions to verify your expertise in angular."
+            title: "Angular Interview Question",
+            desc: "A complete guideline to prepare for angular interviews. Also, you can use these questions to verify your expertise in angular."
       },
       {
-            tittle:"this",
-            desc:"Value of this in Javascript is very confusing and very important. If you are not sure you are a master of this, you have to watch this video..."
+            title: "this",
+            desc: "Value of this in Javascript is very confusing and very important. If you are not sure you are a master of this, you have to watch this video..."
       },
       {
-            tittle:"array",
-            desc:"Array is the mostly used Data stucture. And in Javascript it is at least 3 one of the key data structure that you have to use to master. This video will tell you 15 hidden features that witll make u expert in JavaScript array."
+            title: "array",
+            desc: "Array is the mostly used Data stucture. And in Javascript it is at least 3 one of the key data structure that you have to use to master. This video will tell you 15 hidden features that witll make u expert in JavaScript array."
       },
       {
-            tittle:"React Workshop for beginners",
-            desc:"This is once in a year opportunity for beginners. You will have the opportunity to learn React from the industry expert. You will learn React Fundamentals, React Router and everything else needed to build a professional React App all by yourself with our help."
+            title: "React Workshop for beginners",
+            desc: "This is once in a year opportunity for beginners. You will have the opportunity to learn React from the industry expert. You will learn React Fundamentals, React Router and everything else needed to build a professional React App all by yourself with our help."
       },
       {
-            tittle:"console",
-            desc:"To be a decent web developer you have to know developer tool. If you want to be a better JavaScript developer, you have to master in the console tab of dev tool. there is no exception. no hanky panky..."
+            title: "console",
+            desc: "To be a decent web developer you have to know developer tool. If you want to be a better JavaScript developer, you have to master in the console tab of dev tool. there is no exception. no hanky panky..."
       }
 ]
+
+function loadTutorials() {
+      let tutorialHtml = ``;
+
+      tutorialData.map(tutorial => tutorialHtml += `
+      <div class="tutorial">
+      <h2 class="card__title">${tutorial.title}</h2>
+      <p class="card__text">${tutorial.desc}</p>
+      <button class="card__button">View details >></button>
+      </div>
+      `)
+      document.getElementById('tutorial_container').innerHTML = tutorialHtml
+      // console.log(tutorialHtml);
+}
+loadTutorials();
